d7f8011d73be6b25dd6cec2d75a26d61d4ea9b77
ysw753 <swyoo753@naver.com>
2022-04-05 21:17:56 +0900

    theme
    .d.ts 를 이해해보자

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
 
 import App from './App';
+import { theme } from './theme';
 
 
 ReactDOM.render(
+
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
       <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+
+import { DefaultTheme } from "styled-components";
+
+
+export const theme: DefaultTheme={
+  textColor:"white",
+  bgColor:"#2d3436",
+}
